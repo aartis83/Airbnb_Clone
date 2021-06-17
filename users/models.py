e24f3b9b99f44d2f9a43cbaaeb7b39c6f6e5160f
{Aarti} <{aartisrinivasan83@gmail.com}>
2021-06-17 12:46:20 -0700

    Implements the latest changes to the local host - all working

--- users/models.py
+++ users/models.py
@@ -71,7 +71,7 @@ class User(AbstractUser):
     login_method = models.CharField(
         max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
-    objects = core_managers.CustomModelManager()
+    # objects = core_managers.CustomModelManager()
 
     def get_absolute_url(self):
         return reverse("users:profile", kwargs={"pk": self.pk})
